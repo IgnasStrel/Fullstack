92aeeae1bbd1c700450eb94f312c92738bfec99e
jkunciene <j.kunciene@gmail.com>
2023-06-23 09:34:57 +0300

    create user end point

--- back/index.js
+++ back/index.js
@@ -2,3 +2,16 @@ require('dotenv').config();
 
 const connectDB = require('./config/db');
 connectDB();
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const createNewUser = require('./controllers/usersController');
+
+app.post('/api/user', createNewUser);
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on ${process.env.PORT}`)
+})
